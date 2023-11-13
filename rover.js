9b6dc07a5392af0253ab7c5f845dac00b540cb15
ethandabbs2 <ethandabbs2@yahoo.com>
2023-11-13 14:55:33 -0600

    commit 2

--- rover.js
+++ rover.js
@@ -51,47 +51,5 @@ class Rover {
      }
     }
   }
-//       let sentCommands = message.commands;
-//       let responseCommands = [];
-//       if (!Array.isArray(sentCommands)) {
-//          // Makes sure Commmands are in array form
-//          sentCommands = [sentCommands];   
-//       }
-//       // Loops through the array of commands sent to the rover and applies changes if one of the three command types is issued
-//       for (let i = 0; i < sentCommands.length; i++) {
-//          let completedBoolean = true;     
-
-//          // Variable for the 'completed' key in the rover's response
-//          let statusBoolean = sentCommands[i].commandType === "STATUS_CHECK";
-//          let currentStatus = {};
-//          if (sentCommands[i].commandType === "MODE_CHANGE") {
-//             completedBoolean = (sentCommands[i].value === "NORMAL" || sentCommands[i].value === "LOW_POWER");
-//             this.mode = sentCommands[i].value;
-//          }  else if (sentCommands[i].commandType === "MOVE") {
-//             completedBoolean = (this.mode === "NORMAL");
-//             if (completedBoolean) {
-//                this.position = sentCommands[i].value;
-//             }
-//          }  else if (sentCommands[i].commandType === "STATUS_CHECK") {
-//             currentStatus = {"mode": this.mode, "generatorWatts": this.generatorWatts, "position": this.position};
-//             completedBoolean = statusBoolean;
-//          }  else {
-//             // Indicates a command is not completed if one of the three command types isn't issued
-//             completedBoolean = false;
-//          }
-//          let input = {
-//             "completed": completedBoolean,
-//             // If applicable, adds 'Status Check' to the rover's results' response
-//             ...(statusBoolean && {"roverStatus": currentStatus})
-//          };
-//          responseCommands.push(input);
-//       }
-//       let response = {
-//          "message": message.name,
-//          "results": responseCommands
-//       };
-//       return response;
-//    }
-// } 
 
 module.exports = Rover;
